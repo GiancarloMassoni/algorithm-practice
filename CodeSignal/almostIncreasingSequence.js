6c2d930730e0bf382c1a5e62856381d2a949da01
Giancarlo Massoni <massonicarlo55@gmail.com>
2023-03-22 03:32:31 +0000

    Completed almostIncreasingSequence problem

---
+++ CodeSignal/almostIncreasingSequence.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+function solution(sequence) {
+  let counter = 0;
+  for (let i = 1; i < sequence.length; i++) {
+
+    if (sequence[i - 1] >= sequence[i]) {
+      counter++;
+      if (counter > 1) return false;
+
+      if (sequence[i - 2] >= sequence[i] && sequence[i - 1] >= sequence[i + 1]) return false;
+    }
+
+  }
+
+  return true;
+
+}
